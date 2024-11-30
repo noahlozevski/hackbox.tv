62c01ddcb96fceea41287be82b7a07ab32a30a52
noah lozevski <noahlozevski@gmail.com>
2024-11-30 15:50:39 -0700

    perms in deploy script

--- deploy_static.sh
+++ deploy_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -R /home/noahlozevski/app/site/* /var/www/hackbox_site/
+sudo cp -R /home/noahlozevski/app/site/* /var/www/hackbox_site/
 sudo chown -R www-data:www-data /var/www/hackbox_site
 sudo chmod -R 755 /var/www/hackbox_site
 
